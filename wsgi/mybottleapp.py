f0615e115b6f0e54d5431476a5342f847c5989c2
macgyver13 <ron.gilson@gmail.com>
2014-08-02 08:28:23 -0400

    Fix TEMPLATE_PATH reference
    change reference from HOMEDIR to REPO_DIR

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -11,7 +11,6 @@ def index():
 # This must be added in order to do correct path lookups for the views
 import os
 from bottle import TEMPLATE_PATH
-TEMPLATE_PATH.append(os.path.join(os.environ['OPENSHIFT_HOMEDIR'], 
-    'runtime/repo/wsgi/views/')) 
+TEMPLATE_PATH.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi/views/')) 
 
 application=default_app()
